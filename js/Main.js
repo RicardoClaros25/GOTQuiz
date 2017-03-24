3c26096b6268e1b52800ed04910d36ae88b71313
RicardoClaros <ricardoclaros25@gmail.com>
2017-03-23 23:22:31 -0700

    polished html and java

--- js/Main.js
+++ js/Main.js
@@ -1,35 +1,54 @@
 var questions = [
   {
-    question:"How many Direwolves are left at the end of season 6?"
-    answer:2
+    question:"How many Direwolves are left at the end of season 6?",
+    answer: 2
   },
   {
-    question: "Winter is..."
-    answer: comming
+    question: "Winter is...",
+    answer: "comming"
   },
   {
-    question: "What is the name of Arya's sword?"
-    answer: needle
+    question: "What is the name of Arya's sword?",
+    answer: "needle"
   },
   {
-    question: "Who is the true father of Jon Snow?"
-    answer: Rhaegar Targaryen
-  };
-  {
-    question: "Who killed Oberyn Martell in a duel?"
-    answer: the mountain
+    question: "Who is the true father of Jon Snow?",
+    answer: "Rhaegar Targaryen"
   },
   {
-    question: "A girl is..."
-    answer: Arya Stark
+    question: "Who killed Oberyn Martell in a duel?",
+    answer: "the mountain"
   },
   {
-    question: "What is the name of Daenery's biggest dragon"
-    answer: Drogon
+    question: "A girl is...",
+    answer: "Arya Stark"
+  },
+  {
+    question: "What is the name of Daenery's biggest dragon",
+    answer: "Drogon"
   }
 ]
 
-for(i=0;i < questions.length;i++) {
+for(i = 0; i < questions.length; i++) {
   q = questions[i].question
   document.getElementById('question' + [i]).textContent = q
+}
+
+function testResults() {
+  var correct = 0;
+  var incorrect = 0;
+  for(i = 0; i < questions.length; i++) {
+    var answer = question[i].answer
+    var guess = document.getElementById('answer' + [i]).value
+    var questionSpot = document.getElementById('question' + [i])
+    if(answer == guess) {
+      questionSpot.className = 'correct'
+      correct++
+    } else {
+      questionSpot.className = 'incorrect'
+      incorrect++
+    }
+  }
+  document.getElementById('correct').textContent = correct
+  document.getElementById('incorrect').textContent = incorrect
 }
